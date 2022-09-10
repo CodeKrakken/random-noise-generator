a4121aebacf9ec5451822bb1d1411daf31feb620
CodeKrakken <newholm1@hotmail.com>
2022-09-10 14:38:17 +0100

    Convert frequencies object into array

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import { keys } from './data';
 
 function App() {
 
-  const frequencies = keys.C.major
-
   const context = new AudioContext();
 
   const oscillator10 = context.createOscillator();
@@ -36,15 +34,26 @@ function App() {
   const [maxNoteLength, setMaxNoteLength] = useState(1000);
   const [minFrequency , setMinFrequency ] = useState(20);
   const [maxFrequency , setMaxFrequency ] = useState(20000);
+  const [checked,       setChecked      ] = useState(false)
+  const [activeKeys,    setActiveKeys   ] = useState([])
 
   const getRandomFrequency = () => {
     const minFrequency = +document.getElementById('minFrequency').value
     const maxFrequency = +document.getElementById('maxFrequency').value
-    const filteredFrequencies = frequencies.filter(frequency => frequency >= minFrequency && frequency <= maxFrequency)
+
+    const activeFrequencies = getActiveFrequencies(keys) 
+
+    let filteredFrequencies = activeFrequencies.filter(frequency => frequency >= minFrequency && frequency <= maxFrequency)
     const randomIndex = Math.floor(Math.random()*filteredFrequencies.length)
 
     return filteredFrequencies[randomIndex]
   }
+
+const getActiveFrequencies = (keys) => {
+
+  const outputKeys = Object.keys(keys).map(key => keys[key].major).flat(Infinity)
+  return outputKeys
+}
 
   const stopNote = () => {
     gain10.gain.value = 0
@@ -59,7 +68,6 @@ function App() {
       const waveType = waveTypes[Math.floor(Math.random() * 4)]
       oscillator10.type = waveType
       const frequency = getRandomFrequency();
-      console.log(frequency)
       oscillator10.frequency.value = frequency
       const level = Math.random()
       gain10.gain.value = level
@@ -84,6 +92,17 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault()
+  }
+
+  const handleChange = (e) => {
+    setChecked(!checked)
+    const toggledKey = e.target.value
+    if (activeKeys.includes(toggledKey)) {
+      const filteredKeys = activeKeys.filter(key => key !== toggledKey)
+      setActiveKeys(filteredKeys)
+    } else {
+      activeKeys.push(toggledKey)
+    }
   }
 
   const inputs = [
@@ -121,7 +140,13 @@ function App() {
             />
           )
         }
-      </form>    
+      </form>
+      <input 
+        type="checkbox"
+        value="C"
+        checked={checked}
+        onChange={handleChange}
+      />
     </div>
   );
 }
