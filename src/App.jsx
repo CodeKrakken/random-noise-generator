083d4e0c8f8f2d3368b29823b9831a9bb700e150
CodeKrakken <newholm1@hotmail.com>
2022-10-12 12:26:37 +0100

    Add scale and note selectors

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import ReactDOM from 'react-dom/client';
 import { scales } from './data';
+import { allFrequencies } from './data'
+// import SheetMusic from '@slnsw/react-sheet-music';
 
 let cycling = false
 
@@ -10,18 +12,25 @@ function App() {
   const context = new AudioContext();
 
   const oscillator10 = context.createOscillator();
+  const oscillator20 = context.createOscillator();
+
 
   const gain10 = context.createGain()
+  const gain20 = context.createGain()
 
   oscillator10.connect(gain10);
+  oscillator20.connect(gain20);
+
 
   gain10.connect(context.destination);
+  gain20.connect(context.destination);
+
 
   gain10.gain.value = 0
+  gain20.gain.value = 0
 
   oscillator10.start(0);
-
-  let i = 0
+  oscillator20.start(0);
 
   const waveTypes = [
     'sine',
@@ -36,7 +45,8 @@ function App() {
   const [maxFrequency ,     setMaxFrequency     ] = useState(20000);
   const [checked,           setChecked          ] = useState(false)
   const [activeKeys,        setActiveKeys       ] = useState([])
-  const [activeNumbers,     setActiveNumbers    ] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+  const [activeNotes,     setActiveNotes    ] = useState([1, 3, 5, 6, 8, 10, 12])
+  const [activeScales,      setActiveScales     ] = useState([0,1,2,3,4,5,6,7,8,9,10,11])
   const [cycleButtonLabel,  setCycleButtonLabel ] = useState('Start')
 
   const getRandomFrequency = () => {
@@ -55,23 +65,13 @@ function App() {
 
   const getActiveFrequencies = () => {
 
-    let activeScales = {}
-
-    if (activeKeys.length) {
-
-      activeKeys.map(key => {
-        activeScales[key] = JSON.parse(JSON.stringify(scales[key]))
-      })
-
-    } else {
-      activeScales = JSON.parse(JSON.stringify(scales))
-    }
-
-    const sortedActiveNumbers = activeNumbers.sort((a, b) => b - a)
+    let currentFrequencies = allFrequencies.filter((scale, i) => activeScales.includes(i))
     
-    const frequencyArray = Object.keys(activeScales).map(key => scales[key]).flat(Infinity)
+    let filteredFrequencies = currentFrequencies.map(scale =>
+      scale.filter((note, i) => activeNotes.includes(i+1))
+    )
 
-    return frequencyArray
+    return filteredFrequencies.flat(Infinity)
   }
 
   const stopNote = () => {
@@ -123,7 +123,7 @@ function App() {
     e.preventDefault()
   }
 
-  const handleChange = (e) => {
+  const handleKeyChange = (e) => {
 
     setChecked(!checked)
     const toggledKey = e.target.value
@@ -136,40 +136,59 @@ function App() {
     }
   }
 
-  const handleNumberChange = (e) => {
+  const handleNoteChange = (e) => {
 
     setChecked(!checked)
-    const toggledNumber = +e.target.value
+    const toggledNote = +e.target.value
 
-    if (activeNumbers.includes(toggledNumber)) {
-      const toggledNumberIndex = activeNumbers.indexOf(toggledNumber)
-      activeNumbers.splice(toggledNumberIndex, 1)
+    if (activeNotes.includes(toggledNote)) {
+      const toggledNoteIndex = activeNotes.indexOf(toggledNote)
+      activeNotes.splice(toggledNoteIndex, 1)
     } else {
-      activeNumbers.push(toggledNumber)
+      activeNotes.push(toggledNote)
+    }
+  }
+
+  const handleScaleChange = (e) => {
+
+    setChecked(!checked)
+    const toggledScale = +e.target.value
+
+    if (activeScales.includes(toggledScale)) {
+      const toggledScaleIndex = activeScales.indexOf(toggledScale)
+      activeScales.splice(toggledScaleIndex, 1)
+    } else {
+      activeScales.push(toggledScale)
     }
   }
 
   const inputs = [
     {
+      label:  'Min length',
       id: 'minLength',
       value: minNoteLength,
       action: setMinNoteLength
     },  {
+      label:  'Max length',
       id: 'maxLength',
       value: maxNoteLength,
       action: setMaxNoteLength
-    },  {
+    },  
+    {
+      label: 'Min pitch',
       id: 'minFrequency',
       value:  minFrequency,
       action: setMinFrequency
     },  {
+      label:  'Max pitch',
       id: 'maxFrequency',
       value:  maxFrequency,
       action: setMaxFrequency
     }
   ]
 
-  const numbers = [1,2,3,4,5,6,7,8,9,10,11,12]
+  const notes = [1,2,3,4,5,6,7,8,9,10,11,12]
+  const scales = [0,1,2,3,4,5,6,7,8,9,10]
 
   return (
     <div>
@@ -177,46 +196,59 @@ function App() {
       <button value="Start/Stop" onClick={startStop}>{cycleButtonLabel}</button>
       <form onSubmit={handleSubmit}>
         {
-          inputs.map(input => 
+          inputs.map(input => <>
+            <label>{input.label}</label>
             <input
               id={input.id}
               type="number" 
               value={input.value}
               onChange={(e) => +e !== NaN && input.action(+e.target.value)}
             />
-          )
+          </>)
         }
       </form>
-      {
-        Object.keys(scales).map(key => {
-          const scaleLabels = Object.keys(scales)
-          return <>
-            {scaleLabels.indexOf(key) === Math.ceil(scaleLabels.length/2) && <br/>}
-            <label>{key}</label>
-            <input
-              type="checkbox"
-              value={key}
-              checked={activeKeys.includes(key)}
-              onChange={handleChange}
-            />
-          </>
-        })
-      }
+      
       <br/>
+      Notes
+      <br />
       {
-        numbers.map(number => 
+        notes.map(note => 
           <>
-            <label>{number}</label>
+            <label>{note}</label>
             <input
               type="checkbox"
-              value={number}
-              checked={activeNumbers.includes(number)}
-              onChange={handleNumberChange}
+              value={note}
+              checked={activeNotes.includes(note)}
+              onChange={handleNoteChange}
             />
           </>
         )
       }
+      <br/>
+      Scales
+      <br/>
+      {
+        scales.map(scale =>
+          <>
+            <label>{scale}</label>
+            <input
+              type="checkbox"
+              value={scale}
+              checked={activeScales.includes(scale)}
+              onChange={handleScaleChange}
+            />
+          </>
+        )
+      }
+
+      {/* <SheetMusic
+      // Textual representation of music in ABC notation
+      // The string below will show four crotchets in one bar
+      notation="|EGBF|"
+    /> */}
     </div>
+
+    
   );
 }
 
