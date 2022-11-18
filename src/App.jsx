68677cb22aea78a55177636bc158ef5c4330ff8c
CodeKrakken <newholm1@hotmail.com>
2022-11-18 15:37:21 +0000

    Set up one starting oscillator

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,23 @@ let cycling = false
 
 function App() {
 
-
-
-  const addOscillator = () => {
-    const newOscillator = setUpOscillator()
+  const addOscillator = (bpm) => {
+    const newOscillator = setUpOscillator(bpm)
     const newNodes = [nodes, newOscillator].flat()
     setNodes(newNodes)
   }
 
   const waveShapes = [
     'sine',
-    'sawtooth',
     'triangle',
+    'sawtooth',
     'square'
   ]
 
   const context = new AudioContext();
 
-  const setUpOscillator = () => {
-    const oscillator = context.createOscillator();
+  const setUpOscillator = (bpm) => {
+    const oscillator = context.createOscillator()
     const gain = context.createGain()
     oscillator.connect(gain);
     gain.connect(context.destination);
@@ -36,22 +34,23 @@ function App() {
     const newBpm = nodes.length ? nodes[nodes.length-1].bpm : 120
     
     return {
-      oscillator    : oscillator, 
-      gain          : gain,
-      minFrequency  : 20,
-      maxFrequency  : 20000,
-      bpm           : newBpm,
-      minNoteLength : 500,
-      maxNoteLength : 500,
-      minVolume     : 0,
-      maxVolume     : 100,
-      activeNotes   : [1, 3, 5, 6, 8, 10, 12, 13],
-      activeScales  : [0,1,2,3,4,5,6,7,8,9,10,11]
+      oscillator      : oscillator, 
+      gain            : gain,
+      minFrequency    : 20,
+      maxFrequency    : 20000,
+      bpm             : bpm ?? newBpm,
+      minNoteLength   : 500,
+      maxNoteLength   : 500,
+      minVolume       : 0,
+      maxVolume       : 100,
+      activeNotes     : [1, 3, 5, 6, 8, 10, 12, 13],
+      activeScales    : [0,1,2,3,4,5,6,7,8,9,10,11],
+      activeWaveShapes: waveShapes
     }
   }
+  
 
   const [cycleButtonLabel,  setCycleButtonLabel ] = useState('Start')
-  const [activeWaveShapes,  setActiveWaveShapes ] = useState(waveShapes)
   const [nodes,             setNodes            ] = useState([])
 
   const getRandomFrequency = (i) => {
@@ -84,20 +83,17 @@ function App() {
   }
 
   const playNote = (node, i) => {
-    
     if (cycling)  {
       if (Date.now() >= node.nextNoteAt) {
-        // console.log(`Latency: ${Date.now() - node.nextNoteAt} ms`)
         const minLength   = +document.getElementById(`minLength${i}`).value
-        const maxlength   = +document.getElementById(`maxLength${i}`).value
+        const maxLength   = +document.getElementById(`maxLength${i}`).value
         const bpm         = +document.getElementById(`bpm${i}`).value
-        const noteLength  = bpm ? 60000/bpm : minLength + (Math.random() * (maxlength - minLength))
+        const noteLength  = bpm ? 60000/bpm : minLength + (Math.random() * (maxLength - minLength))
         const minVolume   = +document.getElementById(`minVolume${i}`).value
         const maxVolume   = +document.getElementById(`maxVolume${i}`).value
 
-        const waveShape   = activeWaveShapes[Math.floor(Math.random() * activeWaveShapes.length)]
+        const waveShape   = node.activeWaveShapes[Math.floor(Math.random() * node.activeWaveShapes.length)]
         node.oscillator.type   = waveShape
-
         const frequency   = getRandomFrequency(i);
 
         try {
@@ -107,6 +103,7 @@ function App() {
         }
         
         const level = (minVolume + Math.random() * (maxVolume - minVolume))/100
+        console.log(level)
         node.gain.gain.value = level
         node.nextNoteAt += noteLength
 
@@ -130,17 +127,17 @@ function App() {
     nodes.map(node => {node.gain.gain.value = 0})
   }
   
-  const handleWaveShapeChange = (e) => {
+  // const handleWaveShapeChange = (e) => {
 
-    const toggledWaveShape = e.target.value
+  //   const toggledWaveShape = e.target.value
 
-    if (activeWaveShapes.includes(toggledWaveShape)) {
-      const toggledWaveShapeIndex = activeWaveShapes.indexOf(toggledWaveShape)
-      activeWaveShapes.splice(toggledWaveShapeIndex, 1)
-    } else {
-      activeWaveShapes.push(toggledWaveShape)
-    }
-  }
+  //   if (activeWaveShapes.includes(toggledWaveShape)) {
+  //     const toggledWaveShapeIndex = activeWaveShapes.indexOf(toggledWaveShape)
+  //     activeWaveShapes.splice(toggledWaveShapeIndex, 1)
+  //   } else {
+  //     activeWaveShapes.push(toggledWaveShape)
+  //   }
+  // }
 
   const handleStartStop = () => {
     cycling = !cycling
@@ -152,8 +149,12 @@ function App() {
   const scales  = [0,1,2,3,4,5,6,7,8,9,10]
 
   useEffect(() => {
-    console.log(nodes)
-  }, [nodes])
+    setUpSample()
+  }, [])
+
+  const setUpSample = async () => {
+    addOscillator(64)
+  }
 
   return <>
     <div>
@@ -170,7 +171,6 @@ function App() {
       <br />
       {
         nodes.map((node, i) => {
-          console.log(node.activeNotes)
           return <div className="node">
             <div className="row">
               <div className="column">
@@ -297,8 +297,8 @@ function App() {
                         title={waveShape}
                         type="checkbox"
                         value={waveShape}
-                        checked={activeWaveShapes.includes(waveShape)}
-                        onChange={handleWaveShapeChange}
+                        checked={node.activeWaveShapes.includes(waveShape)}
+                        onChange={(e) => setNodes([nodes.slice(0,i), {...nodes[i], activeWaveShapes: node.activeWaveShapes.includes(e.target.value) ? node.activeWaveShapes.filter(waveShape => waveShape !== e.target.value) : [node.activeWaveShapes, e.target.value].flat()}, nodes.slice(i+1)].flat())}
                       />
                     </>
                   }
