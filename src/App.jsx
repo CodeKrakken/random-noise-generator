e5b6283ab235356c605324edd6c9e841912ea6a9
CodeKrakken <newholm1@hotmail.com>
2022-11-19 12:36:40 +0000

    Add 50% of rest, simplify controls, arrange layout better

--- src/App.jsx
+++ src/App.jsx
@@ -107,100 +107,100 @@ function App() {
     activeScales    : [1,2],
     activeWaveShapes: ['square']
   },
-  {
-    oscillator      : oscillator2, 
-    gain            : gain2,
-    minFrequency    : 20,
-    maxFrequency    : 20000,
-    bpm             : 16,
-    minNoteLength   : 500,
-    maxNoteLength   : 500,
-    minVolume       : 50,
-    maxVolume       : 50,
-    activeNotes     : [1, 4, 6, 8, 11, 13],
-    activeScales    : [3,4,5],
-    activeWaveShapes: ['sine', 'triangle', 'sawtooth']
-  },
-  {
-    oscillator      : oscillator3, 
-    gain            : gain3,
-    minFrequency    : 20,
-    maxFrequency    : 20000,
-    bpm             : 16,
-    minNoteLength   : 500,
-    maxNoteLength   : 500,
-    minVolume       : 50,
-    maxVolume       : 50,
-    activeNotes     : [1, 4, 6, 8, 11, 13],
-    activeScales    : [3,4,5],
-    activeWaveShapes: ['sine', 'triangle', 'sawtooth']
-  },
-  {
-    oscillator      : oscillator4, 
-    gain            : gain4,
-    minFrequency    : 20,
-    maxFrequency    : 20000,
-    bpm             : 16,
-    minNoteLength   : 500,
-    maxNoteLength   : 500,
-    minVolume       : 50,
-    maxVolume       : 50,
-    activeNotes     : [1, 4, 6, 8, 11, 13],
-    activeScales    : [3,4,5],
-    activeWaveShapes: ['sine', 'triangle', 'sawtooth']
-  },
-  {
-    oscillator      : oscillator5, 
-    gain            : gain5,
-    minFrequency    : 20,
-    maxFrequency    : 20000,
-    bpm             : 128,
-    minNoteLength   : 500,
-    maxNoteLength   : 500,
-    minVolume       : 50,
-    maxVolume       : 75,
-    activeNotes     : [1, 3, 4, 6, 8, 9, 11, 13],
-    activeScales    : [6,7],
-    activeWaveShapes: ['triangle']
-  },
-  {
-    oscillator      : oscillator6, 
-    gain            : gain6,
-    minFrequency    : 20,
-    maxFrequency    : 20000,
-    bpm             : 256,
-    minNoteLength   : 500,
-    maxNoteLength   : 500,
-    minVolume       : 75,
-    maxVolume       : 100,
-    activeNotes     : [1, 3, 4, 6, 8, 9, 11, 13],
-    activeScales    : [7,8,9],
-    activeWaveShapes: ['sine']
-  },
-  {
-    oscillator      : oscillator7, 
-    gain            : gain7,
-    minFrequency    : 20,
-    maxFrequency    : 20000,
-    bpm             : 1024,
-    minNoteLength   : 500,
-    maxNoteLength   : 500,
-    minVolume       : 100,
-    maxVolume       : 100,
-    activeNotes     : [1, 3, 4, 6, 8, 9, 11, 13],
-    activeScales    : [10, 11],
-    activeWaveShapes: ['sine', 'triangle']
-  },
+  // {
+  //   oscillator      : oscillator2, 
+  //   gain            : gain2,
+  //   minFrequency    : 20,
+  //   maxFrequency    : 20000,
+  //   bpm             : 16,
+  //   minNoteLength   : 500,
+  //   maxNoteLength   : 500,
+  //   minVolume       : 50,
+  //   maxVolume       : 50,
+  //   activeNotes     : [1, 4, 6, 8, 11, 13],
+  //   activeScales    : [3,4,5],
+  //   activeWaveShapes: ['sine', 'triangle', 'sawtooth']
+  // },
+  // {
+  //   oscillator      : oscillator3, 
+  //   gain            : gain3,
+  //   minFrequency    : 20,
+  //   maxFrequency    : 20000,
+  //   bpm             : 16,
+  //   minNoteLength   : 500,
+  //   maxNoteLength   : 500,
+  //   minVolume       : 50,
+  //   maxVolume       : 50,
+  //   activeNotes     : [1, 4, 6, 8, 11, 13],
+  //   activeScales    : [3,4,5],
+  //   activeWaveShapes: ['sine', 'triangle', 'sawtooth']
+  // },
+  // {
+  //   oscillator      : oscillator4, 
+  //   gain            : gain4,
+  //   minFrequency    : 20,
+  //   maxFrequency    : 20000,
+  //   bpm             : 16,
+  //   minNoteLength   : 500,
+  //   maxNoteLength   : 500,
+  //   minVolume       : 50,
+  //   maxVolume       : 50,
+  //   activeNotes     : [1, 4, 6, 8, 11, 13],
+  //   activeScales    : [3,4,5],
+  //   activeWaveShapes: ['sine', 'triangle', 'sawtooth']
+  // },
+  // {
+  //   oscillator      : oscillator5, 
+  //   gain            : gain5,
+  //   minFrequency    : 20,
+  //   maxFrequency    : 20000,
+  //   bpm             : 128,
+  //   minNoteLength   : 500,
+  //   maxNoteLength   : 500,
+  //   minVolume       : 50,
+  //   maxVolume       : 75,
+  //   activeNotes     : [1, 3, 4, 6, 8, 9, 11, 13],
+  //   activeScales    : [6,7],
+  //   activeWaveShapes: ['triangle']
+  // },
+  // {
+  //   oscillator      : oscillator6, 
+  //   gain            : gain6,
+  //   minFrequency    : 20,
+  //   maxFrequency    : 20000,
+  //   bpm             : 256,
+  //   minNoteLength   : 500,
+  //   maxNoteLength   : 500,
+  //   minVolume       : 75,
+  //   maxVolume       : 100,
+  //   activeNotes     : [1, 3, 4, 6, 8, 9, 11, 13],
+  //   activeScales    : [7,8,9],
+  //   activeWaveShapes: ['sine']
+  // },
+  // {
+  //   oscillator      : oscillator7, 
+  //   gain            : gain7,
+  //   minFrequency    : 20,
+  //   maxFrequency    : 20000,
+  //   bpm             : 1024,
+  //   minNoteLength   : 500,
+  //   maxNoteLength   : 500,
+  //   minVolume       : 100,
+  //   maxVolume       : 100,
+  //   activeNotes     : [1, 3, 4, 6, 8, 9, 11, 13],
+  //   activeScales    : [10, 11],
+  //   activeWaveShapes: ['sine', 'triangle']
+  // },
 ])
 
   const getRandomFrequency = (i) => {
-    const minFrequency = +document.getElementById(`minFrequency${i}`).value
-    const maxFrequency = +document.getElementById(`maxFrequency${i}`).value
-    const activeFrequencies = getActiveFrequencies(nodes[i]) 
-    const filteredFrequencies = activeFrequencies.filter(frequency => frequency >= minFrequency && frequency <= maxFrequency)
-    const randomIndex = Math.floor(Math.random()*filteredFrequencies.length)
+    // const minFrequency = +document.getElementById(`minFrequency${i}`).value
+    // const maxFrequency = +document.getElementById(`maxFrequency${i}`).value
+    let activeFrequencies = getActiveFrequencies(nodes[i]) 
+    // activeFrequencies = activeFrequencies.filter(frequency => frequency >= minFrequency && frequency <= maxFrequency)
+    const randomIndex = Math.floor(Math.random()*activeFrequencies.length)
 
-    return filteredFrequencies[randomIndex]
+    return activeFrequencies[randomIndex]
   }
 
   const getActiveFrequencies = (node) => {
@@ -225,16 +225,17 @@ function App() {
   const playNote = (node, i) => {
     if (cycling)  {
       if (Date.now() >= node.nextNoteAt) {
-        const minLength   = +document.getElementById(`minLength${i}`).value
-        const maxLength   = +document.getElementById(`maxLength${i}`).value
+        // const minLength   = +document.getElementById(`minLength${i}`).value
+        // const maxLength   = +document.getElementById(`maxLength${i}`).value
         const bpm         = +document.getElementById(`bpm${i}`).value
-        const noteLength  = bpm ? 60000/bpm : minLength + (Math.random() * (maxLength - minLength))
+        const noteLength  = 60000/bpm
         const minVolume   = +document.getElementById(`minVolume${i}`).value
         const maxVolume   = +document.getElementById(`maxVolume${i}`).value
 
         const waveShape   = node.activeWaveShapes[Math.floor(Math.random() * node.activeWaveShapes.length)]
         node.oscillator.type   = waveShape
-        const frequency   = getRandomFrequency(i);
+        const frequency   = Math.random() > 0.5 ? getRandomFrequency(i) : 0;
+        console.log(frequency)
 
         try {
           node.oscillator.frequency.value = frequency
@@ -265,18 +266,6 @@ function App() {
     setCycleButtonLabel('Start')
     nodes.map(node => {node.gain.gain.value = 0})
   }
-  
-  // const handleWaveShapeChange = (e) => {
-
-  //   const toggledWaveShape = e.target.value
-
-  //   if (activeWaveShapes.includes(toggledWaveShape)) {
-  //     const toggledWaveShapeIndex = activeWaveShapes.indexOf(toggledWaveShape)
-  //     activeWaveShapes.splice(toggledWaveShapeIndex, 1)
-  //   } else {
-  //     activeWaveShapes.push(toggledWaveShape)
-  //   }
-  // }
 
   const handleStartStop = () => {
     cycling = !cycling
@@ -286,10 +275,6 @@ function App() {
 
   const notes   = [1,2,3,4,5,6,7,8,9,10,11,12,13]
   const scales  = [0,1,2,3,4,5,6,7,8,9,10]
-
-  // useEffect(() => {
-  //   setUpSample()
-  // }, [])
 
   const setUpSample = async () => {
     addOscillator(64)
@@ -313,20 +298,30 @@ function App() {
           return <div className="node">
             <div className="row">
               <div className="column">
-                <input  
-                  title="BPM"
-                  id={`bpm${i}`}
-                  type="number" 
-                  value={node.bpm}
-                  onChange={(e) => setNodes([nodes.slice(0,i), {...nodes[i], bpm: +e.target.value}, nodes.slice(i+1)].flat())}
-                  maxlength={5}
-                  min={0}
-                  max={60000}
-                />
-                  
-                <span>{" "}OR{" "}</span>
-                
+                <div className="row inner-row">BPM</div>
+                <div className="row inner-row">Min lev</div>
+                <div className="row inner-row">Max lev</div>
+              </div>
+
+              <div className="column">
+                <div className="row inner-row">
+                  <input
+                    className='textbox'  
+                    title="BPM"
+                    id={`bpm${i}`}
+                    type="number" 
+                    value={node.bpm}
+                    onChange={(e) => setNodes([nodes.slice(0,i), {...nodes[i], bpm: +e.target.value}, nodes.slice(i+1)].flat())}
+                    maxlength={5}
+                    min={0}
+                    max={60000}
+                  />
+                    
+                {/* <span>{" "}OR{" "}</span>
+                <label></label>
+
                 <input
+                  className='textbox'  
                   title={'min length'}
                   id={`minLength${i}`}
                   type="number" 
@@ -335,17 +330,19 @@ function App() {
                 />
                 ...
                 <input
+                  className='textbox'
                   title={'max length'}
                   id={`maxLength${i}`}
                   type="number" 
                   value={node.maxNoteLength}
                   onChange={(e) => setNodes([nodes.slice(0,i), {...nodes[i], maxNoteLength: +e.target.value}, nodes.slice(i+1)].flat())}
-                />
+                /> */}
                 
                 
-                <br />
+                {/* <br />
 
                 <input
+                  className='textbox'
                   title='Min pitch'
                   id={`minFrequency${i}`}
                   type="number" 
@@ -357,6 +354,7 @@ function App() {
                 />
 
                 <input
+                  className='textbox'
                   title='Max pitch'
                   id={`maxFrequency${i}`}
                   type="number" 
@@ -365,84 +363,94 @@ function App() {
                   maxlength={5}
                   min={0}
                   max={100000}
-                />
+                /> */}
 
-                <br />
-
-                <input
-                  title={'Min volume'}
-                  id={`minVolume${i}`}
-                  type="number" 
-                  value={node.minVolume}
-                  onChange={(e) => setNodes([nodes.slice(0, i), {...nodes[i], minVolume: +e.target.value}, nodes.slice(i+1)].flat())}
-                  min={0}
-                  max={100}
-                  maxlength={3}
-                />
-
-                ...
-
-                <input
-                  title={'Max volume'}
-                  id={`maxVolume${i}`}
-                  type="number" 
-                  value={node.maxVolume}
-                  onChange={(e) => setNodes([nodes.slice(0, i), {...nodes[i], maxVolume: +e.target.value}, nodes.slice(i+1)].flat())}
-                  min={0}
-                  max={100}
-                  maxlength={3}
-                />
+                </div>
+                <div className="row inner-row">
+                  <input
+                    className='textbox'
+                    title={'Min volume'}
+                    id={`minVolume${i}`}
+                    type="number" 
+                    value={node.minVolume}
+                    onChange={(e) => setNodes([nodes.slice(0, i), {...nodes[i], minVolume: +e.target.value}, nodes.slice(i+1)].flat())}
+                    min={0}
+                    max={100}
+                    maxlength={3}
+                  />
+                </div>
+                <div className="row inner-row">
+                  <input
+                    className='textbox'
+                    title={'Max volume'}
+                    id={`maxVolume${i}`}
+                    type="number" 
+                    value={node.maxVolume}
+                    onChange={(e) => setNodes([nodes.slice(0, i), {...nodes[i], maxVolume: +e.target.value}, nodes.slice(i+1)].flat())}
+                    min={0}
+                    max={100}
+                    maxlength={3}
+                  />
+                </div>
               </div>
 
               <div className="column">
-                Notes
-                {
-                  notes.map(note => 
-                    <>
-                      <input
-                        title={note}
-                        type="checkbox"
-                        value={note}
-                        checked={node.activeNotes?.includes(note)}
-                        onChange={(e) => setNodes([nodes.slice(0,i), {...nodes[i], activeNotes: node.activeNotes.includes(+e.target.value) ? node.activeNotes.filter(note => note !== +e.target.value) : [node.activeNotes, +e.target.value].flat()}, nodes.slice(i+1)].flat())}
-                      />
-                    </>
-                  )
-                }
-
-                <br />
-
-                Scales
-
-                {
-                  scales.map(scale =>
-                    <>
-                      <input
-                        title={scale}
-                        type="checkbox"
-                        value={scale}
-                        checked={node.activeScales.includes(scale)}
-                        onChange={(e) => setNodes([nodes.slice(0,i), {...nodes[i], activeScales: node.activeScales.includes(+e.target.value) ? node.activeScales.filter(note => note !== +e.target.value) : [node.activeScales, +e.target.value].flat()}, nodes.slice(i+1)].flat())}
-                      />
-                    </>
-                  )
-                }
-                <br />
-                Wave Shapes
-                { 
-                  waveShapes.map(waveShape => {
-                    return <>
-                      <input
-                        title={waveShape}
-                        type="checkbox"
-                        value={waveShape}
-                        checked={node.activeWaveShapes.includes(waveShape)}
-                        onChange={(e) => setNodes([nodes.slice(0,i), {...nodes[i], activeWaveShapes: node.activeWaveShapes.includes(e.target.value) ? node.activeWaveShapes.filter(waveShape => waveShape !== e.target.value) : [node.activeWaveShapes, e.target.value].flat()}, nodes.slice(i+1)].flat())}
-                      />
-                    </>
+                <div className="row inner-row">
+                  Notes
+                </div>
+                <div className="row inner-row">
+                  Scales
+                </div>
+                <div className="row inner-row">
+                  Waves
+                </div>
+              </div>
+              <div className="column">
+                <div className="row inner-row">
+                  {
+                    notes.map(note => 
+                      <>
+                        <input
+                          title={note}
+                          type="checkbox"
+                          value={note}
+                          checked={node.activeNotes?.includes(note)}
+                          onChange={(e) => setNodes([nodes.slice(0,i), {...nodes[i], activeNotes: node.activeNotes.includes(+e.target.value) ? node.activeNotes.filter(note => note !== +e.target.value) : [node.activeNotes, +e.target.value].flat()}, nodes.slice(i+1)].flat())}
+                        />
+                      </>
+                    )
                   }
-                  )
-                }
+                </div>
+                <div className="row inner-row">
+                  {
+                    scales.map(scale =>
+                      <>
+                        <input
+                          title={scale}
+                          type="checkbox"
+                          value={scale}
+                          checked={node.activeScales.includes(scale)}
+                          onChange={(e) => setNodes([nodes.slice(0,i), {...nodes[i], activeScales: node.activeScales.includes(+e.target.value) ? node.activeScales.filter(note => note !== +e.target.value) : [node.activeScales, +e.target.value].flat()}, nodes.slice(i+1)].flat())}
+                        />
+                      </>
+                    )
+                  }
+                </div>
+                <div className="row inner-row">
+                  { 
+                    waveShapes.map(waveShape => {
+                      return <>
+                        <input
+                          title={waveShape}
+                          type="checkbox"
+                          value={waveShape}
+                          checked={node.activeWaveShapes.includes(waveShape)}
+                          onChange={(e) => setNodes([nodes.slice(0,i), {...nodes[i], activeWaveShapes: node.activeWaveShapes.includes(e.target.value) ? node.activeWaveShapes.filter(waveShape => waveShape !== e.target.value) : [node.activeWaveShapes, e.target.value].flat()}, nodes.slice(i+1)].flat())}
+                        />
+                      </>
+                    })
+                  }
+                </div>
               </div>
               <div className="column">
                 <button 
