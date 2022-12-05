e95aeda613408d6fb5ee6ef2a5b74be2bf2250f7
CodeKrakken <newholm1@hotmail.com>
2022-12-05 10:51:42 +0000

    Factor out oscillator, with many props passed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import { allFrequencies } from './data'
 import useSound from 'use-sound';
 import snareFile  from './sounds/snare.wav';
 import kickFile   from './sounds/kick.wav';
+import Node from './components/Node';
 
 // import SheetMusic from '@slnsw/react-sheet-music';
 
 let cycling = false
 
 function App() {
-
-
 
   const addOscillator = (bpm) => {
     const newOscillator = setUpOscillator(bpm)
@@ -35,14 +34,24 @@ function App() {
   let snare
   let snareSample
 
-  if (typeof document !== 'undefined') {
-    snareSample = document.getElementById("snare");
+  useEffect(() => {
 
-    if (snareSample) {
-      snare = context.createMediaElementSource(snareSample);
-      snare.connect(context.destination)
+    const getSnareSample = async () => {
+      if (typeof document !== 'undefined') {
+        const snareSample = await document.getElementById("snare");
+      
+        if (snareSample) {
+          snare = context.createMediaElementSource(snareSample);
+          snare.connect(context.destination)
+          return snareSample
+
+        }
+      }
     }
-  }
+    
+    snareSample = getSnareSample()
+  
+  }, [])
 
   const setUpOscillator = (bpm) => {
     const oscillator = context.createOscillator()
@@ -288,42 +297,48 @@ function App() {
 
         const liveWaves = Array.from(document.getElementsByClassName(`wave${i}`)).filter(wave => wave.checked)
 
-        const waveShape   = liveWaves[Math.floor(Math.random() * liveWaves.length)].value
-        node.oscillator.type   = waveShape
-
-        const chanceOfRest        = +document.getElementById(`rest${i}`).value/100
-        const diceRoll = Math.random()
-        const frequency   = diceRoll >= chanceOfRest ? getRandomFrequency(i) : 0;
-
-        if (
-          [
-            'sine',
-            'triangle',
-            'sawtooth',
-            'square',
-          ]
-          .includes(waveShape)
-        ) {
-          try {
-            node.oscillator.frequency.value = frequency
-
-            const level       = (minVolume + Math.random() * (maxVolume - minVolume))/100
-            const noteLengthPercentage  = (minLength + Math.random() * (maxLength - minLength))
-            node.gain.gain.value = level/nodes.length
-            node.nextNoteAt += intervalLength
-            const noteLength = intervalLength / 100 * noteLengthPercentage
-
-            if (noteLength < intervalLength) {
-
-              setTimeout(() => {node.gain.gain.value = 0}, noteLength)
-          
+        if (liveWaves) {
+          const waveShape   = liveWaves[Math.floor(Math.random() * liveWaves.length)].value
+          node.oscillator.type   = waveShape
+  
+          const chanceOfRest        = +document.getElementById(`rest${i}`).value/100
+          const diceRoll = Math.random()
+          const frequency   = diceRoll >= chanceOfRest ? getRandomFrequency(i) : 0;
+  
+          if (
+            [
+              'sine',
+              'triangle',
+              'sawtooth',
+              'square',
+            ]
+            .includes(waveShape)
+          ) {
+            try {
+              node.oscillator.frequency.value = frequency
+  
+              const level       = (minVolume + Math.random() * (maxVolume - minVolume))/100
+              const noteLengthPercentage  = (minLength + Math.random() * (maxLength - minLength))
+              node.gain.gain.value = level/nodes.length
+              node.nextNoteAt += intervalLength
+              const noteLength = intervalLength / 100 * noteLengthPercentage
+  
+              if (noteLength < intervalLength) {
+  
+                setTimeout(() => {node.gain.gain.value = 0}, noteLength)
+            
+              }
+            } catch (error) {
+              console.log(error)
             }
-          } catch (error) {
-            console.log(error)
+          } else { 
+            try {
+              snareSample.play()
+            } catch (error) {
+              console.log(error.message)
+            }  
           }
-        } else {       snareSample.play()
         }
-        
       }
 
       setTimeout(() => {playNote(node, i)}, interval - latency)
@@ -360,7 +375,7 @@ function App() {
   const scales  = [0,1,2,3,4,5,6,7,8,9,10]
 
   return <>
-    <audio id="snare" src="src/sounds/snare.wav"></audio>
+    <div id="snare" src="src/sounds/snare.wav"></div>
     <div>
       RANDOM NOISE GENERATOR{" "}
       <button 
@@ -373,193 +388,19 @@ function App() {
       <button onClick={addOscillator}>Add Oscillator</button>
 
       <br />
-      {
-        nodes.map((node, i) => {
-          return <div className="node">
-            <div className="row">
-              <div className="column">
-                <div className="row inner-row">BPM</div>
-                <div className="row inner-row">Min level</div>
-                <div className="row inner-row">Max level</div>
-                <div className="row inner-row">Min length</div>
-                <div className="row inner-row">Max length</div>
-                <div className="row inner-row">Rest %</div>
-              </div>
-
-              <div className="column">
-                <div className="row inner-row">
-                  <input
-                    className='textbox'  
-                    title="BPM"
-                    id={`bpm${i}`}
-                    type="number" 
-                    value={node.bpm}
-                    onChange={(e) => setNodes([nodes.slice(0,i), {...nodes[i], bpm: +e.target.value}, nodes.slice(i+1)].flat())}
-                    maxlength={5}
-                    min={0}
-                    max={60000}
-                  />
-                </div>
-                <div className="row inner-row">
-                  <input
-                    className='textbox'
-                    title={'Min volume'}
-                    id={`minVolume${i}`}
-                    type="number" 
-                    value={node.minVolume}
-                    onChange={(e) => setNodes([nodes.slice(0, i), {...nodes[i], minVolume: +e.target.value}, nodes.slice(i+1)].flat())}
-                    min={0}
-                    max={100}
-                    maxlength={3}
-                  />
-                </div>
-                <div className="row inner-row">
-                  <input
-                    className='textbox'
-                    title={'Max volume'}
-                    id={`maxVolume${i}`}
-                    type="number" 
-                    value={node.maxVolume}
-                    onChange={(e) => setNodes([nodes.slice(0, i), {...nodes[i], maxVolume: +e.target.value}, nodes.slice(i+1)].flat())}
-                    min={0}
-                    max={100}
-                    maxlength={3}
-                  />
-                </div>
-                <div className="row inner-row">
-                  <input
-                    className='textbox'  
-                    title={'min length'}
-                    id={`minLength${i}`}
-                    type="number"
-                    min={0}                     
-                    max={100} 
-                    maxlength={3}
-                    value={node.minNoteLength}
-                    onChange={(e) => setNodes([nodes.slice(0,i), {...nodes[i], minNoteLength: +e.target.value}, nodes.slice(i+1)].flat())}
-                  />
-                </div>
-
-                <div className="row inner-row">
-                  <input
-                    className='textbox'
-                    title={'max length'}
-                    id={`maxLength${i}`}
-                    type="number"
-                    min={0} 
-                    max={100} 
-                    maxlength={3}
-                    value={node.maxNoteLength}
-                    onChange={(e) => setNodes([nodes.slice(0,i), {...nodes[i], maxNoteLength: +e.target.value}, nodes.slice(i+1)].flat())}
-                  />
-                </div>
-
-                <div className="row inner-row">
-                  <input
-                    className='textbox'
-                    title={'Rest %'}
-                    id={`rest${i}`}
-                    type="number" 
-                    value={node.rest}
-                    onChange={(e) => setNodes([nodes.slice(0, i), {...nodes[i], rest: +e.target.value}, nodes.slice(i+1)].flat())}
-                    min={0}
-                    max={100}
-                    maxlength={3}
-                  />
-                </div>
-              </div>
-
-              <div className="column">
-                <div className="row inner-row">
-                  Notes
-                </div>
-                <div className="row inner-row">
-                  Scales
-                </div>
-                <div className="row inner-row">
-                  Waves
-                </div>
-                <div>
-                  Intervals
-                </div>
-              </div>
-              <div className="column">
-                <div className="row inner-row">
-                  {
-                    notes.map(note => 
-                      <>
-                        <input
-                          className={`note${i}`}
-                          title={note}
-                          type="checkbox"
-                          value={note}
-                          checked={node.activeNotes?.includes(note)}
-                          onChange={(e) => setNodes([nodes.slice(0,i), {...nodes[i], activeNotes: node.activeNotes.includes(+e.target.value) ? node.activeNotes.filter(note => note !== +e.target.value) : [node.activeNotes, +e.target.value].flat()}, nodes.slice(i+1)].flat())}
-                        />
-                      </>
-                    )
-                  }
-                </div>
-                <div className="row inner-row">
-                  {
-                    scales.map(scale =>
-                      <>
-                        <input
-                          className={`scale${i}`}
-                          title={scale}
-                          type="checkbox"
-                          value={scale}
-                          checked={node.activeScales.includes(scale)}
-                          onChange={(e) => setNodes([nodes.slice(0,i), {...nodes[i], activeScales: node.activeScales.includes(+e.target.value) ? node.activeScales.filter(note => note !== +e.target.value) : [node.activeScales, +e.target.value].flat()}, nodes.slice(i+1)].flat())}
-                        />
-                      </>
-                    )
-                  }
-                </div>
-                <div className="row inner-row">
-                  { 
-                    waveShapes.map(waveShape => {
-                      return <>
-                        <input
-                          title={waveShape}
-                          className={`wave${i}`}
-                          type="checkbox"
-                          value={waveShape}
-                          checked={node.activeWaveShapes.includes(waveShape)}
-                          onChange={(e) => setNodes([nodes.slice(0,i), {...nodes[i], activeWaveShapes: node.activeWaveShapes.includes(e.target.value) ? node.activeWaveShapes.filter(waveShape => waveShape !== e.target.value) : [node.activeWaveShapes, e.target.value].flat()}, nodes.slice(i+1)].flat())}
-                        />
-                      </>
-                    })
-                  }
-                </div>
-                <div className="row inner-row">
-                  { 
-                    intervals.map(interval => {
-                      return <>
-                        <input
-                          className={`interval${i}`}
-                          title={interval}
-                          type="checkbox"
-                          value={interval}
-                          checked={node.activeIntervals.includes(interval)}
-                          onChange={(e) => setNodes([nodes.slice(0,i), {...nodes[i], activeIntervals: node.activeIntervals.includes(+e.target.value) ? node.activeIntervals.filter(interval => interval !== +e.target.value) : [node.activeIntervals, +e.target.value].flat()}, nodes.slice(i+1)].flat())}
-                        />
-                      </>
-                    })
-                  }
-                </div>
-              </div>
-              <div className="column">
-                <button 
-                  id={`delete-node${i}`} 
-                  onClick={(e) => handleDelete(i, e)}
-                >X</button>
-              </div>
-            </div>
-          </div>
-          
-        })
-      }
+      {nodes.map((node, i) => 
+        <Node 
+          node={node} 
+          i={i} 
+          setNodes    = {setNodes} 
+          nodes       = {nodes}
+          notes       = {notes}
+          scales      = {scales}
+          waveShapes  = {waveShapes}
+          intervals   = {intervals}
+          handleDelete= {handleDelete}
+        />
+      )}
     </div>
   </>
 }
