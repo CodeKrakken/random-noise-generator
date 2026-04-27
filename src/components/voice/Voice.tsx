1168fb4fe73c1e16a9df91bb0d9daf85b012a45d
CodeKrakken <newholm1@hotmail.com>
2026-04-27 14:26:36 +0100

    WIP refactoring Attributes object

--- src/components/voice/Voice.tsx
+++ src/components/voice/Voice.tsx
@@ -44,228 +44,225 @@ export default function Voice(props: VoiceProps) {
         onChange= {(e: any) => updateVoice(e, 'label')}
       />
     ],
-    // label: [
-    //   {
-    //     className: 'textbox',  
-    //     title: "Label",
-    //     label: "Name",
-    //     id: `label${i}`,
-    //     'data-testid': `voice-label-${i}`,
-    //     type: "text",
-    //     value: voice.label,
-    //     onChange: (e: any) => updateVoice(e, 'label')
-    //   }
-    // ],
-    // bpm: [
-    //   {
-    //     className: 'textbox',
-    //     title: "BPM",
-    //     label: "BPM",
-    //     id: `bpm${i}`,
-    //     type: "number",
-    //     value: voice.bpm,
-    //     onChange: (e: any) => updateVoice(e, 'bpm'),
-    //     maxLength: 5,
-    //     min: 0,
-    //     max: 60000
-    //   }
-    // ],
-    // restChance: [
-    //   {
-    //     className: 'textbox',
-    //     title: 'Rest %',
-    //     label: 'Rest %',
-    //     id: `rest${i}`,
-    //     type: "number",
-    //     value: voice.rest,
-    //     onChange: (e: any) => updateVoice(e, 'rest'),
-    //     min:0,
-    //     max:100,
-    //     maxLength:3
-    //   }
-    // ],
-    // level: [
-    //   {
-    //     className: 'textbox',
-    //     title: 'Min Level',
-    //     label: 'Level',
-    //     id: `minLevel${i}`,
-    //     type: "number",
-    //     value: voice.minLevel,
-    //     onChange: (e: any) => updateVoice(e, 'minLevel'),
-    //     min:0,
-    //     max:100,
-    //     maxLength:3
-    //   },
-    //   {
-    //     className: 'textbox',
-    //     title: 'Max Level',
-    //     id: `maxLevel${i}`,
-    //     type: "number",
-    //     value: voice.maxLevel,
-    //     onChange: (e: any) => updateVoice(e, 'maxLevel'),
-    //     min:0,
-    //     max:100,
-    //     maxLength:3
-    //   }
-    // ],
-    // length: [
-    //   {
-    //     className: 'textbox',
-    //     title: 'Min Length',
-    //     label: 'Length',
-    //     id: `minLength${i}`,
-    //     type: "number",
-    //     value: voice.minNoteLength,
-    //     onChange: (e: any) => updateVoice(e, 'minNoteLength'),
-    //     min:0,
-    //     max:100,
-    //     maxLength:3
-    //   },
-    //   {
-    //     className: 'textbox',
-    //     title: 'Max Length',
-    //     id: `maxLength${i}`,
-    //     type: "number",
-    //     value: voice.maxNoteLength,
-    //     onChange: (e: any) => updateVoice(e, 'maxNoteLength'),
-    //     min:0,
-    //     max:100,
-    //     maxLength:3
-    //   }
-    // ],
-    // offset: [
-    //   {
-    //     className: 'textbox',
-    //     title: 'Min Offset',
-    //     label: 'Offset',
-    //     id: `minOffset${i}`,
-    //     type: "number",
-    //     value: voice.minOffset,
-    //     onChange: (e: any) => updateVoice(e, 'minOffset'),
-    //     min:0,
-    //     max:100,
-    //     maxLength:3
-    //   },
-    //   {
-    //     className: 'textbox',
-    //     title: 'Max Offset',
-    //     id: `maxOffset${i}`,
-    //     type: "number",
-    //     value: voice.maxOffset,
-    //     onChange: (e: any) => updateVoice(e, 'maxOffset'),
-    //     min:0,
-    //     max:100,
-    //     maxLength:3
-    //   }
-    // ],
-    // detune: [
-    //   {
-    //     className: 'textbox',
-    //     title: 'detune',
-    //     label: 'Detune',
-    //     id: `minDetune${i}`,
-    //     type: "number",
-    //     value: voice.minDetune,
-    //     onChange: (e: any) => updateVoice(e, 'minDetune'),
-    //     min:-100,
-    //     max:100,
-    //     maxLength:4
-    //   },
-    //   {
-    //     className: 'textbox',
-    //     title: 'detune',
-    //     id: `maxDetune${i}`,
-    //     type: "number",
-    //     value: voice.maxDetune,
-    //     onChange: (e: any) => updateVoice(e, 'maxDetune'),
-    //     min:-100,
-    //     max:100,
-    //     maxLength:4
-    //   }
-    // ],
-    // fadeIn: [
-    //   {
-    //     className: 'textbox',
-    //     title: 'Min Fade In',
-    //     label: 'Fade In',
-    //     id: `minFadeIn${i}`,
-    //     type: "number",
-    //     value: voice.minFadeIn,
-    //     onChange: (e: any) => updateVoice(e, 'minFadeIn'),
-    //     maxLength:4
-    //   },
-    //   {
-    //     className: 'textbox',
-    //     title: 'Max Fade In',
-    //     id: `maxFadeIn${i}`,
-    //     type: "number",
-    //     value: voice.maxFadeIn,
-    //     onChange: (e: any) => updateVoice(e, 'maxFadeIn'),
-    //     maxLength:4
-    //   }
-    // ],
-    // fadeOut: [
-    //   {
-    //     className: 'textbox',
-    //     title: 'Min Fade Out',
-    //     label: 'Fade Out',
-    //     id: `minFadeOut${i}`,
-    //     type: "number",
-    //     value: voice.minFadeOut,
-    //     onChange: (e: any) => updateVoice(e, 'minFadeOut'),
-    //     maxLength:4
-    //   },
-    //   {
-    //     className: 'textbox',
-    //     title: 'Max Fade Out',
-    //     id: `maxFadeOut${i}`,
-    //     type: "number",
-    //     value: voice.maxFadeOut,
-    //     onChange: (e: any) => updateVoice(e, 'maxFadeOut'),
-    //     maxLength:4
-    //   }
-    // ],
-    // notes: allNotes.map((note, j) => {
-    //   return {
-    //     className: `note${i}`,
-    //     title: note.toString(),
-    //     type: "checkbox",
-    //     value: note,
-    //     checked: voice.activeNotes?.includes(note),
-    //     onChange: (e: any) => updateCheckbox(e, 'activeNotes')
-    //   }
-    // }),
-    // octaves: allOctaves.map((octave, j) => {
-    //   return {
-    //     className: `octave${i}`,
-    //     title: octave.toString(),
-    //     type: "checkbox",
-    //     value: octave,
-    //     checked: voice.activeOctaves.includes(octave),
-    //     onChange: (e: any) => updateCheckbox(e, 'activeOctaves')
-    //   }
-    // }),
-    // waveShapes: allWaveShapes.map((waveShape, j) => {
-    //   return {
-    //     title: waveShape,
-    //     className: `wave${i}`,
-    //     type: "checkbox",
-    //     value: waveShape,
-    //     checked: voice.activeWaveShapes.includes(waveShape),
-    //     onChange: (e: any) => updateCheckbox(e, 'activeWaveShapes')
-    //   }
-    // }),
-    // intervals: allIntervals.map((interval, j) => {
-    //   return {
-    //     className: `interval${i}`,
-    //     title: interval.toString(),
-    //     type: "checkbox",
-    //     value: interval,
-    //     checked: voice.activeIntervals.includes(interval),
-    //     onChange: (e: any) => updateCheckbox(e, 'activeIntervals')
-    //   }
-    // })
+    bpm: [
+      <Input
+        className= 'textbox'
+        title= "BPM"
+        label= "BPM"
+        id= {`bpm${i}`}
+        i={i}
+        type= "number"
+        value= {voice.bpm}
+        onChange= {(e: any) => updateVoice(e, 'bpm')}
+        maxLength= {5}
+        min= {0}
+        max= {60000}
+      />
+    ],
+    restChance: [
+      <Input
+        className= 'textbox'
+        title= 'Rest %'
+        label= 'Rest %'
+        id= {`rest${i}`}
+        i={i}
+        type= "number"
+        value= {voice.rest}
+        onChange= {(e: any) => updateVoice(e, 'rest')}
+        min= {0}
+        max= {100}
+        maxLength= {3}
+      />
+    ],
+    level: [
+      <Input
+        className= 'textbox'
+        title= 'Min Level'
+        label= 'Level'
+        id= {`minLevel${i}`}
+        i={i}
+        type= "number"
+        value= {voice.minLevel}
+        onChange= {(e: any) => updateVoice(e, 'minLevel')}
+        min= {0}
+        max= {100}
+        maxLength= {3}
+      />
+      <Input
+        className= 'textbox'
+        title= 'Max Level'
+        label= 'Level'
+        id= `maxLevel${i}`
+        type= "number"
+        value= {voice.maxLevel}
+        onChange= {(e: any) => updateVoice(e, 'maxLevel')}
+        min= {0}
+        max= {100}
+        maxLength= {3} 
+      />
+    ],
+    length: [
+      <Input
+        className= 'textbox'
+        title= 'Min Length'
+        label= 'Length'
+        id= `minLength${i}`
+        type= "number"
+        value= {voice.minNoteLength}
+        onChange= {(e: any) => updateVoice(e, 'minNoteLength')]
+        min= {0}
+        max= {100}
+        maxLength= {3}
+      />,
+      <Input
+        className= 'textbox'
+        title= 'Max Length'
+        label= 'Length'
+        id= `maxLength${i}`
+        type= "number"
+        value= {voice.maxNoteLength}
+        onChange= {(e: any) => updateVoice(e, 'maxNoteLength')
+        min= {0}
+        max= {100}
+        maxLength= {3}
+      />
+    ],
+    offset: [
+      <Input
+        className= 'textbox'
+        title= 'Min Offset'
+        label= 'Offset'
+        id= `minOffset${i}`
+        type= "number"
+        value= {voice.minOffset}
+        onChange= {(e: any) => updateVoice(e, 'minOffset')
+        min= {0}
+        max= {100}
+        maxLength= {3}
+      />,
+      <Input
+        className= 'textbox'
+        title= 'Max Offset'
+        label= 'Offset'
+        id= `maxOffset${i}`
+        type= "number"
+        value= {voice.maxOffset}
+        onChange= {(e: any) => updateVoice(e, 'maxOffset')
+        min= {0}
+        max= {100}
+        maxLength= {3}
+      />
+    ],
+    detune: [
+      <Input
+        className= 'textbox'
+        title= 'detune'
+        label= 'Detune'
+        id= `minDetune${i}`
+        type= "number"
+        value= {voice.minDetune}
+        onChange= {(e: any) => updateVoice(e, 'minDetune')
+        min= {-100}
+        max= {100}
+        maxLength= {4}
+      />,
+      <Input
+        className= 'textbox'
+        title= 'detune'
+        label= 'Detune'
+        id= `maxDetune${i}`
+        type= "number"
+        value= {voice.maxDetune}
+        onChange= {(e: any) => updateVoice(e, 'maxDetune')
+        min= {-100}
+        max= {100}
+        maxLength= {4}
+      />
+    ],
+    fadeIn: [
+      <Input
+        className= 'textbox'
+        title= 'Min Fade In'
+        label= 'Fade In'
+        id= `minFadeIn${i}`
+        type= "number"
+        value= {voice.minFadeIn}
+        onChange= {(e: any) => updateVoice(e, 'minFadeIn')
+        maxLength= {4}
+      />,
+      <Input
+        className= 'textbox'
+        title= 'Max Fade In'
+        label= 'Fade In'
+        id= `maxFadeIn${i}`
+        type= "number"
+        value= {voice.maxFadeIn}
+        onChange= {(e: any) => updateVoice(e, 'maxFadeIn')
+        maxLength= {4}
+      />
+    ],
+    fadeOut: [
+      <Input
+        className= 'textbox'
+        title= 'Min Fade Out'
+        label= 'Fade Out'
+        id= `minFadeOut${i}`
+        type= "number"
+        value= {voice.minFadeOut}
+        onChange= {(e: any) => updateVoice(e, 'minFadeOut')
+        maxLength= {4}
+      />,
+      <Input
+        className= 'textbox'
+        title= 'Max Fade Out'
+        label= 'Fade Out'
+        id= `maxFadeOut${i}`
+        type= "number"
+        value= {voice.maxFadeOut}
+        onChange= {(e: any) => updateVoice(e, 'maxFadeOut')
+        maxLength= {4}
+      />
+    ],
+    notes: allNotes.map((note, j) => {
+      return <Input
+        className= `note${i}`
+        title= note.toString()
+        type= "checkbox"
+        value= note
+        checked= {voice.activeNotes?.includes(note)}
+        onChange= (e: any) => updateCheckbox(e, 'activeNotes')
+      />
+    }),
+    octaves: allOctaves.map((octave, j) => {
+      return <Input
+        className= `octave${i}`
+        title= octave.toString()
+        type= "checkbox"
+        value= octave
+        checked= {voice.activeOctaves.includes(octave)}
+        onChange= (e: any) => updateCheckbox(e, 'activeOctaves')
+      />
+    }),
+    waveShapes: allWaveShapes.map((waveShape, j) => {
+      return <Input
+        className= `wave${i}`
+        title= waveShape
+        type= "checkbox"
+        value= waveShape
+        checked= {voice.activeWaveShapes.includes(waveShape)}
+        onChange= (e: any) => updateCheckbox(e, 'activeWaveShapes')
+      />
+    }),
+    intervals: allIntervals.map((interval, j) => {
+      return <Input
+        className= `interval${i}`
+        title= interval.toString()
+        type= "checkbox"
+        value= interval
+        checked= {voice.activeIntervals.includes(interval)}
+        onChange= (e: any) => updateCheckbox(e, 'activeIntervals')
+      />
+    })
   }
 
   return <div 
