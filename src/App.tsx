803e659d9fad8cbb0470435642eb36254d41002b
CodeKrakken <newholm1@hotmail.com>
2026-05-09 17:17:31 +0100

    Move functions out of App component

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,39 @@ const active = (voices: voice[]) => {
   return voices.filter(voice => voice.isActive)
 }
 
+const setUpVoice = (template: voice | null = null) => {
+  return {
+    isActive        : true,
+    label           : template?.label!+1          ||  1,
+    nextInterval    : 0,
+    bpm             : template?.bpm              ??  120,
+    minLevel        : template?.minLevel         ??  100,
+    maxLevel        : template?.maxLevel         ??  100,
+    activeNotes     : template?.activeNotes      ??  ['1','3','5','6','8','10','12','13'],
+    activeOctaves   : template?.activeOctaves    ??  ['4'],
+    activeWaveforms : template?.activeWaveforms  ??  ['sine'],
+    restChance      : template?.restChance       ??  0,
+    activeIntervals : template?.activeIntervals  ??  ['0.5'],
+    minLength       : template?.minLength        ??  100,
+    maxLength       : template?.maxLength        ??  100,
+    minOffset       : template?.minOffset        ??  0,  
+    maxOffset       : template?.maxOffset        ??  0,
+    minDetune       : template?.minDetune        ??  0,
+    maxDetune       : template?.maxDetune        ??  0,
+    minFadeIn       : template?.minFadeIn        ??  100,
+    maxFadeIn       : template?.maxFadeIn        ??  100,
+    minFadeOut      : template?.minFadeOut       ??  100,
+    maxFadeOut      : template?.maxFadeOut       ??  100,
+  }
+}
+
+const setUpSample = (file: string, context: AudioContext) => {
+  const sample = new Audio(file)
+  const sound = context.createMediaElementSource(sample);
+  sound.connect(context.destination)
+  return sample
+}
+
 function App() {
 
   const [context] = useState(() => new AudioContext())
@@ -20,44 +53,9 @@ function App() {
   const addVoice = () => {
     setVoices(voices => [voices, setUpVoice(active(voices).reverse()[0])].flat())
   }
-
-  const setUpVoice = (templateVoice: voice | null = null) => {
-    
-
-    return {
-      isActive        : true,
-      label           : templateVoice?.label!+1          ||  1,
-      nextInterval    : 0,
-      bpm             : templateVoice?.bpm              ??  120,
-      minLevel        : templateVoice?.minLevel         ??  100,
-      maxLevel        : templateVoice?.maxLevel         ??  100,
-      activeNotes     : templateVoice?.activeNotes      ??  ['1','3','5','6','8','10','12','13'],
-      activeOctaves   : templateVoice?.activeOctaves    ??  ['4'],
-      activeWaveforms : templateVoice?.activeWaveforms  ??  ['sine'],
-      restChance      : templateVoice?.restChance       ??  0,
-      activeIntervals : templateVoice?.activeIntervals  ??  ['0.5'],
-      minLength       : templateVoice?.minLength        ??  100,
-      maxLength       : templateVoice?.maxLength        ??  100,
-      minOffset       : templateVoice?.minOffset        ??  0,  
-      maxOffset       : templateVoice?.maxOffset        ??  0,
-      minDetune       : templateVoice?.minDetune        ??  0,
-      maxDetune       : templateVoice?.maxDetune        ??  0,
-      minFadeIn       : templateVoice?.minFadeIn        ??  100,
-      maxFadeIn       : templateVoice?.maxFadeIn        ??  100,
-      minFadeOut      : templateVoice?.minFadeOut       ??  100,
-      maxFadeOut      : templateVoice?.maxFadeOut       ??  100,
-    }
-  }
-
-  const setUpSample = (file: string) => {
-    const sample = new Audio(file)
-    const sound = context.createMediaElementSource(sample);
-    sound.connect(context.destination)
-    return sample
-  }
-
-  const snareSample = setUpSample(snareFile)
-  const kickSample  = setUpSample(kickFile)
+ 
+  const snareSample = setUpSample(snareFile, context)
+  const kickSample  = setUpSample(kickFile, context)
 
   const handleStartStop = async () => {
     running ? stop() : start()
