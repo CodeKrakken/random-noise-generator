bf8773f7e608770bed30a030d7f35ba1422a105d
CodeKrakken <newholm1@hotmail.com>
2026-07-30 14:33:16 +0100

    Test for Header handleSave functionality - no more red on Header

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,5 +1,6 @@
-import { render, screen } from '@testing-library/react';  
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';  
 import Header from './Header';  
+import { makeVoice } from '../../shared.test.functions';
   
 jest.mock('../../content/data', () => ({  
   title:    'OCTOPUS',  
@@ -16,7 +17,7 @@ describe('Header', () => {
       handleAddVoice={mockHandleAddVoice}  
       handleStartStop={mockHandleStartStop}  
       running={state}  
-      voices={[]}  
+      voices={[makeVoice()]}  
       loadVoices={mockLoadVoices}  
     />  
   )  
@@ -35,4 +36,14 @@ describe('Header', () => {
     expect(screen.queryByRole('button', { name: 'S t a r t' })).not.toBeInTheDocument();  
     expect(screen.getByRole('button',   { name: 'S t o p'   })).toBeInTheDocument();  
   });  
+
+  it('enables Load button after Save', () => {
+    render(header(false))
+
+    const loadButton = screen.getByRole('button', { name: 'L o a d' });
+
+    expect(loadButton).toBeDisabled()
+    fireEvent.click(screen.getByRole('button', { name: 'S a v e'}));  
+    expect(loadButton).toBeEnabled();
+  })
 });
