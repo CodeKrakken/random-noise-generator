2da585c2ee281779bff36af800c14753a1f20496
CodeKrakken <newholm1@hotmail.com>
2026-08-02 18:28:41 +0100

    Add VexFlow music staves

---
+++ src/components/Timeline/Timeline.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useRef } from 'react'
+import { Renderer, Stave } from 'vexflow'
+
+const Timeline = () => {
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (!containerRef.current) return
+
+    const renderer = new Renderer(
+      containerRef.current,
+      Renderer.Backends.SVG
+    )
+
+    renderer.resize(500, 200)
+
+    const context = renderer.getContext()
+
+    const stave = new Stave(10, 40, 400)
+
+    stave
+      .addClef('treble')
+      .setContext(context)
+      .draw()
+  }, [])
+
+  return <div ref={containerRef} />
+}
+
+export default Timeline
