82981c75698cde15f10221245c74e10865022908
CodeKrakken <newholm1@hotmail.com>
2026-08-30 19:26:45 +0100

    Restore native right click function and add showDetails toggle to left click

--- src/components/Hit/Hit.tsx
+++ src/components/Hit/Hit.tsx
@@ -4,40 +4,25 @@ import HitDetails from "../HitDetails/HitDetails"
 
 export default function Hit({
 
-  frequencies,
-  pixelsPerSecond,
   key,
   hit,
-  voices,
-  frequencyToPixels
 
 } : {
 
-  frequencies: number[]
-  pixelsPerNote: number
-  pixelsPerSecond: number
   key: string
   hit: HitType
-  voices: VoiceType[]
-  frequencyToPixels: (frequency: number) => number
 
 }) {
 
   const [showDetails, setShowDetails] = useState(false)
 
-  const handleLeftClick = (e: React.MouseEvent<HTMLDivElement>) => setShowDetails(false)
-
-  const handleRightClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    e.preventDefault()
-    setShowDetails(!showDetails)
-  }
+  const handleLeftClick = (e: React.MouseEvent<HTMLDivElement>) => setShowDetails(!showDetails)
 
   return <>
     <div
       key={key}
       className="hit"
       onClick={handleLeftClick}
-      onContextMenu={handleRightClick}
       style={hit.style}
     >
       {
